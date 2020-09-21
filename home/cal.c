a8f4b8bd04fe1a82c0fc0adcdb71b81448e6abf9
longkangJack <1030269471@qq.com>
2020-09-21 22:01:14 +0800

    gengxin

---
+++ home/cal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"mymath.h"
+int main()
+{
+  int result=Add(1,2);
+  printf("result = %d\n",result);
+  return 0;
+}
---
+++ home/mymath.c
@@ -0,0 +1,5 @@
+#include"mymath.h"
+int Add(int x,int y)
+{
+  return x+y;
+}
